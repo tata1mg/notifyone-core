3de49082804e9ccc235d5ab2d9d0fd1e2c94a2a6
Prashant.Mishra <prashant.mishra@C02FD0YHMD6M.local>
2024-10-07 19:30:24 +0530

    AES encrption for sensitive providers data

--- app/utilities/crypto.py
+++ app/utilities/crypto.py
@@ -7,13 +7,15 @@ from Crypto.Cipher import AES
 
 class Crypto:
 
-    @staticmethod
-    def encrypt(message):
-        obj = AES.new(CONFIG.config.get('ENCRYPTION_KEY'), AES.MODE_CFB, CONFIG.config.get('AES_IV'))
+    PROVIDERS_DATA_ENCRYPTION = CONFIG.config.get("PROVIDERS", {}).get("ENCRYPTION")
+
+    @classmethod
+    def encrypt(cls, message):
+        obj = AES.new(cls.PROVIDERS_DATA_ENCRYPTION["KEY"], AES.MODE_CFB, cls.PROVIDERS_DATA_ENCRYPTION["AES_IV"])
         ciphertext = obj.encrypt(message)
         return str(base64.b64encode(ciphertext), 'utf-8')
 
-    @staticmethod
-    def decrypt(encrypted_message):
-        obj = AES.new(CONFIG.config.get('ENCRYPTION_KEY'), AES.MODE_CFB, CONFIG.config.get('AES_IV'))
+    @classmethod
+    def decrypt(cls, encrypted_message):
+        obj = AES.new(cls.PROVIDERS_DATA_ENCRYPTION["KEY"], AES.MODE_CFB, cls.PROVIDERS_DATA_ENCRYPTION["AES_IV"])
         return obj.decrypt(base64.b64decode(encrypted_message)).decode('utf-8')
